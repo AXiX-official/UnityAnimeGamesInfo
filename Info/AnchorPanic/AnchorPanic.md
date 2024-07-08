27276143ad0f67bb1e066e27cd5c9f94a937a99a
AXiX-official <2879710747@qq.com>
2024-07-08 15:12:57 +0800

    Update AnchorPanic

--- Info/AnchorPanic/AnchorPanic.md
+++ Info/AnchorPanic/AnchorPanic.md
@@ -1,3 +1,7 @@
 # 锚点降临(Anchor Panic)
 
 移动端是il2cpp但是PC端是mono
+
+## LuaScripts
+
+使用的DES加密，解密使用的脚本[decrypt.py](decrypt.py)
---
+++ Info/AnchorPanic/decrypt.py
@@ -0,0 +1,45 @@
+import sys
+import os
+import hashlib
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import unpad
+
+mCommonKey = "rfizzapj"
+mCommonKey = hashlib.md5(mCommonKey.encode()).hexdigest()
+mCommonKey = mCommonKey[-8:]
+#mDesMKeys = [111, 151, 50, 205, 123, 222, 185, 45]
+mDesMKeys = b'\x6f\x97\x32\xcd\x7b\xde\xb9\x2d'
+
+def DecryptBytes(bytes, key) :
+    key = key.encode()
+    cipher = DES.new(key, DES.MODE_CBC, mDesMKeys)
+    decrypted_data = unpad(cipher.decrypt(bytes), DES.block_size)
+    return decrypted_data
+
+def DecryptFile(source, target) :
+    with open(source, "rb") as f :
+        data = f.read()
+    decrypted_data = DecryptBytes(data, mCommonKey)
+    with open(target, "wb") as f :
+        f.write(decrypted_data)
+
+def Decrypt(source, target) :
+    if not os.path.exists(target) :
+        os.makedirs(target)
+    for root, dirs, files in os.walk(source):
+        for file in files:
+            source_file = os.path.join(root, file)
+            target_file = os.path.join(target, file)
+            DecryptFile(source_file, target_file)
+
+
+if __name__ == "__main__" :
+    if sys.argv.__len__() < 3 :
+        print("Usage: python decrypt.py source target")
+        sys.exit()
+    source = sys.argv[1]
+    target = sys.argv[2]
+    if os.path.isdir(source) :
+        Decrypt(source, target)
+    else :
+        DecryptFile(source, target)
