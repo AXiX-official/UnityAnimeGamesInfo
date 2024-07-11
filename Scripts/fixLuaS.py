491ad82a70c98ebeeaae5afa44bf20cd1be145fb
AXiX-official <2879710747@qq.com>
2024-07-11 15:29:39 +0800

    fix fixLuaS.py

--- Scripts/fixLuaS.py
+++ Scripts/fixLuaS.py
@@ -8,7 +8,7 @@ FIXED_HEAD = (b'\x1B\x4C\x75\x61\x53\x00\x19\x93\x0D\x0A\x1A\x0A\x04\x04\x04\x08
 unluac_path = "unluac.jar"
 
 
-def fixFile(src: str, dst: str) -> str:
+def fixFile(src: str, dst: str):
     '''
     修复luac文件的文件头
     '''
@@ -17,20 +17,19 @@ def fixFile(src: str, dst: str) -> str:
     data = FIXED_HEAD + data[HEAD_LEN:]
     with open(dst, 'wb') as f:
         f.write(data)
-    return dst
 
-def fixDir(src: str, dst: str) -> list:
+def fixDir(src: str, dst: str):
     files = os.walk(src)
     tmp_files = []
     for root, _, filenames in files:
         for filename in filenames:
             src_file = os.path.join(root, filename)
-            dst_file = src_file.replace(src, dst)
+            relative_path = os.path.relpath(src_file, src)
+            dst_file = os.path.join(dst, relative_path)
             dst_dir = os.path.dirname(dst_file)
             if not os.path.exists(dst_dir):
                 os.makedirs(dst_dir)
-            tmp_files.append(fixFile(src_file, dst_file))
-    return tmp_files
+            fixFile(src_file, dst_file)
 
 
 def decompile(src: str, dst: str) -> bool:
@@ -50,11 +49,11 @@ if __name__ == '__main__':
     if len(sys.argv) == 4:
         unluac_path = str(sys.argv[3])
     if os.path.isfile(src):
-        tmp = fixFile(src, dst)
-        decompile(tmp, dst)
-        os.remove(tmp)
+        fixFile(src, f'{src}.tmp')
+        decompile(f'{src}.tmp', dst)
+        os.remove(f'{src}.tmp')
     else:
-        os.makedirs(".tmp")
-        tmp_files = fixDir(src, ".tmp")
-        decompile(".tmp", dst)
-        os.system(f'rm -rf .tmp')
+        os.makedirs(f'{src}_tmp')
+        fixDir(src, f'{src}_tmp')
+        decompile(f'{src}_tmp', dst)
+        os.system(f'rm -rf {src}_tmp')
